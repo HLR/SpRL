4606e4461869e474a45a0fc76d53016706a5bc02
Taher Rahgooy <taher.rahgooy@hotmail.com>
2017-12-04 00:30:10 -0600

    more functionality added to the annotator.

--- annotator/app.js
+++ annotator/app.js
@@ -3,7 +3,7 @@ var App = function () {
     self.images = ko.observableArray([]);
     self.currentImageIndex = ko.observable(-1);
     self.currentImage = ko.observable({
-        relations:[]
+        relations: []
     });
     self.currentPhrase = ko.observable({
     });
@@ -12,12 +12,12 @@ var App = function () {
 
     self.setCurrentPhrase = function (id) {
         self.currentPhrase({
-            resetSeg: function() {
+            resetSeg: function () {
             }
         });
         var image = self.currentImage();
-        for(var i in image.phrases){
-            if(image.phrases[i].id == id){
+        for (var i in image.phrases) {
+            if (image.phrases[i].id == id) {
                 self.currentPhrase(image.phrases[i]);
                 break;
             }
@@ -34,6 +34,30 @@ var App = function () {
         var images = Object.keys(m.images).map(function (key) { return m.images[key]; });
         self.images(images);
     };
+
+    self.getAnnotations = function () {
+        var images = self.images();
+        var txt = "";        
+        for (var i in images) {
+            var phrases = images[i].phrases;
+            for (var j in phrases) {
+                var p = phrases[j];
+                txt += p.imFolder + "\t\t";
+                txt += p.imId + "\t\t";
+                txt += p.sentId + "\t\t";
+                txt += p.sentence + "\t\t";
+                txt += p.start + "\t\t";
+                txt += p.end + "\t\t";
+                txt += p.text + "\t\t";
+                txt += p.segId + "\t\t";
+                txt += p.segX + "\t\t";
+                txt += p.segY + "\t\t";
+                txt += p.segWidth + "\t\t";
+                txt += p.segHeight + "\r\n";
+            }
+        }
+        return txt;
+    }
 };
 
 var app = new App();
